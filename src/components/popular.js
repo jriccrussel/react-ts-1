cb8c2bc06def1d40e5fc07665270acf679c446d7
jriccrussel <jriccrussel@gmail.com>
2022-05-26 09:23:03 +0800

    popular component | localStorage

--- src/components/popular.js
+++ src/components/popular.js
@@ -12,10 +12,18 @@ export const Popular = () => {
     }, [])    
 
     const getPopular = async () => {
-        const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`)
-        const data = await api.json()
-        // console.log(data)
-        setPopular(data.recipes)
+
+        const check = localStorage.getItem("popular")
+
+        if (check){
+            setPopular(JSON.parse(check))
+        } else{
+            const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`)
+            const data = await api.json()
+            localStorage.setItem("popular", JSON.stringify(data.recipes))
+            setPopular(data.recipes)
+            console.log(data.recipes)
+        }
     }
 
     return (
