3ebdbad62cb3d2e2af19dd4b9d5ca8c2b1cdec31
jriccrussel <jriccrussel@gmail.com>
2022-05-26 08:43:28 +0800

    popular component | fetch && display data

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React, { useContext, useEffect } from 'react'
-import logo from './logo.svg'
 import './App.css'
 import { AppContext } from './context/context'
+import { Home } from './pages/home'
 
 function App() {
   const context = useContext(AppContext)
-  const { states }:any = context
+  const { states }:any = context   
   const { searchVAlue, isLoading, open,openTwo, close } = states
 
   useEffect(() => {
@@ -15,20 +15,7 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Home />
     </div>
   )
 }
---
+++ src/pages/home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Popular } from '../components/popular'
+import { Veggie } from '../components/veggie'
+
+export const Home = () => {
+    return (
+        <>
+            <Veggie />
+            <Popular />
+        </>
+    )
+}
